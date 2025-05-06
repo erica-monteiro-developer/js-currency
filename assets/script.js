5a06bd5fc0bc170b9cbbf5662deb2137ea2e9d27
erica-monteiro-developer <00.erica.al.monteiro@gmail.com>
2025-05-06 07:56:25 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+let dolar = 5.1;
+convert()
+
+let usdInput = document.querySelector("#usd");
+let brlInput = document.querySelector("#brl");
+
+usdInput.addEventListener("keyup",() => {
+
+});
+
+brlInput.addEventListener("keyup",() => {
+
+});
+
+usdInput.value = "1000,00";
+convert("usd-to-brl");
+
+function formatCurrency(value){
+
+}
+
+function convert(type){
+    if((type == "usd-to-br")){
+        // ajustar o valor
+        // converter o valor
+        // mostra no campo do real
+
+    }
+    if((type == "brl-to-usd")){
+        // ajustar o valor
+        // converter o valor
+        // mostra no campo do dólar
+    }
+}
+
+1:51:11
